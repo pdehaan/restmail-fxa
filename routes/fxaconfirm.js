95f4f587c16f1855983fe50f6761d2be5a9e6d8b
Peter deHaan <peter@deseloper.com>
2015-05-13 13:08:38 -0700

    updating stage auth-server uri

--- routes/fxaconfirm.js
+++ routes/fxaconfirm.js
@@ -21,7 +21,7 @@ function verifyCode (server, uid, code) {
   // Map the content server URLs to their auth server endpoints.
   var ENDPOINTS = {
     'accounts.firefox.com': 'api.accounts.firefox.com',
-    'accounts.stage.mozaws.net': 'api-accounts-stage.mozaws.net',
+    'accounts.stage.mozaws.net': 'api-accounts.stage.mozaws.net',
     'stable.dev.lcip.org': 'stable.dev.lcip.org/auth',
     'nightly.dev.lcip.org': 'nightly.dev.lcip.org/auth',
     'latest.dev.lcip.org': 'latest.dev.lcip.org/auth'
